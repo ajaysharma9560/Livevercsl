f265dde8981854049e36f2dbfcf9f236b3e83c93
ajaysharma9560 <75651714+ajaysharma9560@users.noreply.github.com>
2026-06-16 08:48:24 +0530

    Create apk.js

---
+++ api/apk.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
+        success: true,
+        message: "APK Proxy is working"
+    });
+}
